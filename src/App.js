aba303c94024b0caf78d45720f84e5669dc1ab8a
Yash Sanghavi <y.sanghavi001@gmail.com>
2021-09-17 18:42:41 +0530

    Complete workingwithchart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ResponsiveChart from "./components/ResponsiveChart";
 
 function App() {
+  const dataArray = [3000, 2000, 1000];
+  const dataLabel = ["January", "February", "March"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Demo Of How to Customize Chart JS</h1>
+      <ResponsiveChart dataArray={dataArray} dataLabel={dataLabel} />
     </div>
   );
 }
